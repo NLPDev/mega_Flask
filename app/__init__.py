d2f95ff0e7e406dc7aeb8a4101c511c32f64b80b
NLPDev <vasile123andronic@gmail.com>
2020-01-30 05:28:33 -0800

    translate date and time

--- app/__init__.py
+++ app/__init__.py
@@ -55,4 +55,5 @@ if not app.debug:
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
     
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, g
+from flask_login import current_user, login_user, logout_user, login_required
+from flask_babel import get_locale
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, ResetPasswordRequestForm, ResetPasswordForm
 from app.email import send_password_reset_email
-from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Post
 from datetime import datetime
 
@@ -157,3 +158,7 @@ def unfollow(username):
     db.session.commit()
     flash('You are not following {}'.format(username))
     return redirect(url_for('user', username=username))
+
+@app.before_request
+def before_request():
+    g.locale = str(get_locale())
