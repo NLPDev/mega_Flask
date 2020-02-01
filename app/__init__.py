eff18912cd31194ab3785575d774010b52f8e052
NLPDev <vasile123andronic@gmail.com>
2020-01-31 17:59:09 -0800

    blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -17,43 +17,62 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
-from app import routes, models, errors
+from app import routes, models
 
-if not app.debug:
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMIN'], subject='Microblog Failure',
-            credentials=auth, secure=secure
-        )
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-    # if not os.path.exists('logs'):
-    #     os.mkdir('logs')
-    # file_handler = RotatingFileHandler('logs/mega.log', maxBytes=10240, backupCount=10)
-    # file_handler.setFormatter(logging.Formatter(
-    #     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
-    # file_handler.setLevel(logging.INFO)
-    # app.logger.addHandler(file_handler)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    babel.init_app(app)
 
-    # app.logger.setLevel(logging.INFO)
-    # app.logger.info('Mega startup')
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+    if not app.debug and not app.testing:
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMIN'], subject='Microblog Failure',
+                credentials=auth, secure=secure
+            )
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        # if not os.path.exists('logs'):
+        #     os.mkdir('logs')
+        # file_handler = RotatingFileHandler('logs/mega.log', maxBytes=10240, backupCount=10)
+        # file_handler.setFormatter(logging.Formatter(
+        #     '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+        # file_handler.setLevel(logging.INFO)
+        # app.logger.addHandler(file_handler)
+
+        # app.logger.setLevel(logging.INFO)
+        # app.logger.info('Mega startup')
+
+    @babel.localeselector
+    def get_locale():
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
     
--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,7 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, PostForm, Re
 from app.email import send_password_reset_email
 from app.models import User, Post
 from datetime import datetime
+from guess_language import guess_language
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
@@ -14,7 +15,10 @@ from datetime import datetime
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
